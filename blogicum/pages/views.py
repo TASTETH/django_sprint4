d21f5813ad315e782111e0763bc772c86cdb4193
Ruslan Dzarasov <ruslandzarasov835@gmail.com>
2025-05-29 19:16:38 +0300

    as

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+
 def csrf_failure(request, reason=''):
     return render(request, 'pages/403csrf.html', status=403)
 
