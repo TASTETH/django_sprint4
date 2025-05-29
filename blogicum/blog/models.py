c55263ed9dcf8e0cf3ea1d1ec03b4a2f85f8bc7b
Ruslan Dzarasov <ruslandzarasov835@gmail.com>
2025-05-29 18:56:07 +0300

    pep8 again

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -94,9 +94,12 @@ class Post(BaseModel):
 
     def __str__(self):
         return self.title
-    
+
+
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments'
+    )
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
